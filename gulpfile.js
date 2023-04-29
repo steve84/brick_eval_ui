2f5cbdb90872707a079aff83e3144e9bdde79572
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2023-04-29 12:13:55 +0200

    Add gulp file to minifiy css and html, copy fomantic css and fonts

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const minifycss = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const pump = require('pump');
+
+
+gulp.task('copy-main-css', (cb) => {
+  pump([
+    gulp.src('./node_modules/fomantic-ui/dist/semantic.min.css'),
+	gulp.dest('./static/public/static/css')
+  ], cb);
+});
+
+gulp.task('copy-component-css', (cb) => {
+  pump([
+    gulp.src('./node_modules/fomantic-ui/dist/components/*.min.css'),
+	gulp.dest('./static/public/static/css/components')
+  ], cb);
+});
+
+gulp.task('copy-theme-fonts', (cb) => {
+  pump([
+    gulp.src('./node_modules/fomantic-ui/dist/themes/default/**/*'),
+	gulp.dest('./static/public/static/css/themes/default')
+  ], cb);
+});
+
+gulp.task('js-compress', (cb) => {
+  pump([
+    gulp.src('./static/public/**/*.js'),
+    uglify(),
+    gulp.dest('./static/public')
+  ], cb);
+});
+
+gulp.task('minify-css', (cb) => {
+  pump([
+    gulp.src('./static/public/**/*.css'),
+    minifycss({ compatibility: 'ie8' }),
+    gulp.dest('./static/public')
+  ], cb);
+});
+
+gulp.task('minify-html', (cb) => {
+  pump([
+    gulp.src('./static/public/**/*.html'),
+    htmlmin({
+      minifyJS : true,
+      minifyCSS : true,
+      minifyURLs : true,
+      removeComments : true,
+      removeRedundantAttributes : true,
+      sortAttributes : false,
+      sortClassName : false
+    }),
+    gulp.dest('./static/public')
+  ], cb);
+});
+
+gulp.task('compress', gulp.series('copy-main-css', 'copy-component-css', 'copy-theme-fonts', 'js-compress', 'minify-css', 'minify-html'));
+
+gulp.task('default', gulp.series('compress'));
